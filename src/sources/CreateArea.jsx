b1fff618d795fa285fabdb62dedcc02f4ed19416
Vedat Ozkan <vedatkemalozkan@gmail.com>
2021-01-15 19:27:23 -0500

    update on styling

--- src/sources/CreateArea.jsx
+++ src/sources/CreateArea.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
   
@@ -6,6 +9,8 @@ function CreateArea(props) {
     title: "",
     content: ""
   });
+
+  const [mouseIn, updateMouse] = useState(false);
 
 
   function handleChange(event) {
@@ -23,10 +28,14 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form className="form">
-        <input onChange={handleChange} name="title" placeholder="Title" value={note.title}/>
-        <textarea onChange={handleChange}  name="content" placeholder="Take a note..." rows="3" value={note.content} />
-        <button type="button" onClick={() => props.addArray({title: note.title, content: note.content})}>Add</button>
+      <form className="create-note">
+        {mouseIn && <input onChange={handleChange} name="title" placeholder="Title" value={note.title}/>}
+        <textarea onChange={handleChange} onClick={() => updateMouse(true)} name="content" placeholder="Take a note..." rows={mouseIn ? "3" : "1"} value={note.content} />
+        <Zoom in={mouseIn}>
+          {<Fab type="button" onClick={() => props.addArray({title: note.title, content: note.content})}>
+            <AddIcon />
+          </Fab>}
+        </Zoom>
       </form>
     </div>
   );
--- src/sources/note.jsx
+++ src/sources/note.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function createNote(props) {
   return (<div className="note">
     <h1>{props.title}</h1>
     <p>{props.content}</p>
-    <button type="button" onClick={() => props.deleteItem(props.id)}>DELETE</button>
+    <button type="button" onClick={() => props.deleteItem(props.id)}>
+      <DeleteIcon />
+    </button>
   </div>)
 };
 
